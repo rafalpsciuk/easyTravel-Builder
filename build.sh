afacae9237bf8ea97ee3c86c296c23d2e74d2d50
Martin Etmajer <m.etmajer@gmail.com>
2016-04-15 15:50:19 +0200

    Update build.sh to reflect most recent changes to easyTravel source distros.

--- build.sh
+++ build.sh
@@ -6,33 +6,31 @@ ET_SRC_CF_HOME="${ET_SRC_HOME}/CustomerFrontend"
 ET_SRC_BB_HOME="${ET_SRC_HOME}/BusinessBackend"
 ET_SRC_LG_HOME="${ET_SRC_HOME}/com.dynatrace.uemload"
 
-ET_DEPLOY_HOME=${ET_DEPLOY_HOME:-$(pwd)/deploy}
+ET_DEPLOY_HOME="${ET_DEPLOY_HOME:-$(pwd)/deploy}"
 ET_CF_DEPLOY_HOME="${ET_DEPLOY_HOME}/${ET_CF_DEPLOY_HOME:-frontend}"
 ET_BB_DEPLOY_HOME="${ET_DEPLOY_HOME}/${ET_BB_DEPLOY_HOME:-backend}"
 ET_LG_DEPLOY_HOME="${ET_DEPLOY_HOME}/${ET_LG_DEPLOY_HOME:-loadgen}"
 ET_CF_DEPLOY_LIB_HOME="${ET_CF_DEPLOY_HOME}/lib"
 
-cd ${ET_SRC_HOME}
+cd "${ET_SRC_HOME}"
 
 # Download easyTravel sources
-curl -L -o easyTravel-src.zip ${ET_SRC_URL}
+curl -L -o easyTravel-src.zip "${ET_SRC_URL}"
 
 # Unarchive and build easyTravel sources
 unzip ./easyTravel-src.zip
 ant -f ./Distribution war
-cd ${ET_SRC_HOME}/Distribution/dist
+cd "${ET_SRC_HOME}/Distribution/dist"
 
 # Deploy easyTravel build artifacts into the workspace
-mkdir -p ${ET_BB_DEPLOY_HOME}
-cp -v ./business/business.war ${ET_BB_DEPLOY_HOME}
+mkdir -p "${ET_BB_DEPLOY_HOME}"
+cp -v ./business/backend.war ${ET_BB_DEPLOY_HOME}
 
-mkdir -p ${ET_CF_DEPLOY_HOME} ${ET_CF_DEPLOY_LIB_HOME}
-cp -v ./customer/customer.war                    ${ET_CF_DEPLOY_HOME}
-cp -v ./customer/webapp/WEB-INF/lib/jsf-api.jar  ${ET_CF_DEPLOY_LIB_HOME}
-cp -v ./customer/webapp/WEB-INF/lib/jsf-impl.jar ${ET_CF_DEPLOY_LIB_HOME}
+mkdir -p "${ET_CF_DEPLOY_HOME}"
+cp -v ./customer/frontend.war ${ET_CF_DEPLOY_HOME}
 
-mkdir -p ${ET_LG_DEPLOY_HOME}
-tar -cvzf ${ET_LG_DEPLOY_HOME}/loadgen.tar.gz \
+mkdir -p "${ET_LG_DEPLOY_HOME}"
+tar -cvzf "${ET_LG_DEPLOY_HOME}/loadgen.tar.gz" \
           ./lib/commons-cli-*.jar \
           ./lib/commons-lang*.jar \
           ./lib/commons-logging-*.jar \
