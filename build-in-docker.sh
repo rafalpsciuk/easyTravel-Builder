e9a573368611554f535390787065af40cd7fd0f9
Martin Etmajer <m.etmajer@gmail.com>
2016-04-18 17:07:15 +0200

    Add DOCKER_CONTAINER_BUILD_SH_PREFIX.

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,17 +1,18 @@
-DOCKER_WORKDIR=/workspace
+DOCKER_CONTAINER_BUILD_SH_PREFIX=.
+DOCKER_CONTAINER_WORKDIR=/workspace
 
 if [ ! -z "${ET_DEPLOY_HOME}" ]; then
   # Make sure that ET_DEPLOY_HOME gets a location inside the user's current working
-  # directory, which gets mounted into as the Docker container's DOCKER_WORKDIR.
-  ET_DEPLOY_HOME="${DOCKER_WORKDIR}/${ET_DEPLOY_HOME}"
+  # directory, which gets mounted into as the Docker container's DOCKER_CONTAINER_WORKDIR.
+  ET_DEPLOY_HOME="${DOCKER_CONTAINER_WORKDIR}/${ET_DEPLOY_HOME}"
 fi
 
 docker run --rm \
-  --volume "$(pwd):${DOCKER_WORKDIR}" \
+  --volume "$(pwd):${DOCKER_CONTAINER_WORKDIR}" \
   --env ET_SRC_URL="${ET_SRC_URL}" \
   --env ET_DEPLOY_HOME="${ET_DEPLOY_HOME}" \
   --env ET_BB_DEPLOY_HOME="${ET_BB_DEPLOY_HOME}" \
   --env ET_CF_DEPLOY_HOME="${ET_CF_DEPLOY_HOME}" \
   --env ET_LG_DEPLOY_HOME="${ET_LG_DEPLOY_HOME}" \
   monostream/apache-ant \
-  ./build.sh
+  "${DOCKER_CONTAINER_BUILD_SH_PREFIX}/build.sh"
